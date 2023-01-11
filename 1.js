d0ba0497800e28a82fd73aaf84ce3e459ef0c63e
CitrusYx <115406214+CitrusYx@users.noreply.github.com>
2023-01-12 00:40:59 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,41 @@
+var label = document.getElementsByTagName("label");
+var word = document.getElementsByClassName('word');
+var bg = document.getElementById("bg");
+
+label[0].onclick = function () {
+    for (var i = 0; i < word.length; i++) {
+        word[i].style.display = "none";
+    }
+    word[0].style.display = "block";
+}
+
+label[1].onclick = function () {
+    for (var i = 0; i < word.length; i++) {
+        word[i].style.display = "none";
+    }
+    word[1].style.display = "block";
+}
+label[2].onclick = function () {
+    for (var i = 0; i < word.length; i++) {
+        word[i].style.display = "none";
+    }
+    word[2].style.display = "block";
+}
+label[3].onclick = function () {
+    for (var i = 0; i < word.length; i++) {
+        word[i].style.display = "none";
+    }
+    word[3].style.display = "block";
+}
+label[4].onclick = function () {
+    for (var i = 0; i < word.length; i++) {
+        word[i].style.display = "none";
+    }
+    word[4].style.display = "block";
+}
+label[5].onclick = function () {
+    for (var i = 0; i < word.length; i++) {
+        word[i].style.display = "none";
+    }
+    word[5].style.display = "block";
+}
